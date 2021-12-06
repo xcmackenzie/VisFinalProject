0e2cd48f7eeb5ab0a9061bdbfc76c41da7708e36
gracemyers <myersgb@bc.edu>
2021-12-05 20:28:17 -0500

    1980 text

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ d3.csv("data/bcdemographics.csv", d3.autoType).then(demographics => {
         d3.csv("data/bcStudentsbyState.csv", d3.autoType).then(states => {
             d3.json("data/us-states.json", d3.autoType).then(statemap => {
                 d3.csv("data/statepopulation.csv", d3.autoType).then(population => {
+                    d3.csv("data/1980USpop.csv", d3.autoType).then(pop => {   
                 //console.log(demographics)
                 //console.log(schools)
                 //console.log(states)
@@ -39,19 +40,24 @@ d3.csv("data/bcdemographics.csv", d3.autoType).then(demographics => {
                 }
 
                 // Maps
-                var filtered = states.filter(d => d.Year == 1976 || d.Year == 2020)
+                var filtered = states.filter(d => d.Year == 1980 || d.Year == 2020)
                 var domain = d3.extent(filtered, d => d.Value)
 
                 const mapUS = USMap(".map-us", population, statemap)
                 mapUS.on_events()
-                const map1976 = Map(".map-1976", states, statemap, 1976, domain)
-                map1976.on_events(states)
+                const map1980 = Map(".map-1980", states, statemap, 1980, domain)
+                map1980.on_events(states)
                 const map2020 = Map(".map-2020", states, statemap, 2020, domain)
                 map2020.on_events(states)
 
-                map1976.on("click", (clicked) => {
+                map1980.on("click", (clicked) => {
                     let filtered = states.filter(d => d.Location == clicked)
+                    let filter2020 = filtered.filter(d => d.Year == 1980)
                     let values = filtered.map(d => d.Value)
+                    let bcPercent = filter2020.map(d => d.Percent) *100
+
+                    let usfilter = population.filter(d => d.State == clicked)
+                    let usPercent = usfilter.map(d => d.Percent) *100
 
                     document.getElementById("state-name").innerHTML = clicked
 
@@ -65,7 +71,12 @@ d3.csv("data/bcdemographics.csv", d3.autoType).then(demographics => {
                         2010: ${values[2]} students<br>
                         2015: ${values[1]} students<br>
                         2020: ${values[0]} students`
+                    
+                    document.getElementById("map-comp").innerHTML = 
+                        `In 1980 ${usPercent}% of the US population is from ${clicked} compared to ${(bcPercent)}% of the BC student body`
+                        
                 })
+
 
                 map2020.on("click", (clicked) => {
                     let filtered = states.filter(d => d.Location == clicked)
@@ -92,7 +103,8 @@ d3.csv("data/bcdemographics.csv", d3.autoType).then(demographics => {
                         
                         document.getElementById("map-comp").innerHTML = 
                         `In 2020 ${usPercent}% of the US population is from ${clicked} compared to ${(bcPercent)}% of the BC student body`
-                    
+                        
+                        })
                     })
                 })
             })
